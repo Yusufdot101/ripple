bd653b5b41e29116c6d0c1a75db0d1cad128797c
yusuf mohamed <yusuf.mohamed.work@gmail.com>
2026-04-12 18:05:48 +0300

    made logged in users be redirected to the chats page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,16 @@
+"use client";
+import { useAuthStore } from "@/store/useAuthStore";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 const Home = () => {
+    const router = useRouter();
+    const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
+    useEffect(() => {
+        if (isLoggedIn) {
+            router.push("/chats");
+        }
+    }, [isLoggedIn]);
     return <div className="flex flex-col gap-y-[8px] text-center">Home</div>;
 };
 
